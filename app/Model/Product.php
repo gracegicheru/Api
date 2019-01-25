5239f6a570fa17788a87be420f263e6dfc8ee088
gracegicheru <gracegicheru3@gmail.com>
2019-01-25 12:20:59 +0300

    relationship created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Model;
-
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function Review(){
+        return $this->hasMany(Review::class);
+    }
 }

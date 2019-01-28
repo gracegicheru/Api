e20256b4ad06cd0d9922cef94d134dbb52e03970
gracegicheru <gracegicheru3@gmail.com>
2019-01-28 16:34:49 +0300

    modified products details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,12 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock==0 ? 'Out of Stock': $this->stock,
+            'discount'=>$this->discount,
+            'totalPrice'=>round((1-($this->discount/100))*$this->price),
+            'href'=>[
+                'reviews'=>route('reviews.index', $this->id)
+            ]
         ];
     }
 }

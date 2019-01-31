bdd5e65b583147b5bc34037afa18c1b58408d8e0
gracegicheru <gracegicheru3@gmail.com>
2019-01-31 15:13:52 +0300

    passport package

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
-
-class ProductCollection extends ResourceCollection
+use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Facades\Route;
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,16 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            /*
+            'name'=>$this->name,
+            'total price'=>round((1-($this->discount/100))*$this->price),
+            'href'=>[
+                'link'=>route ('product.show', $this->id)
+            ]
+            */
+        ];
+        
+        
     }
 }
--- app/User.php
+++ app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -24,7 +25,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
+    protected $fileable = [
+        'name','email', 'password',
     ];
 }
